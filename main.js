f5d08d06f1d6a5a5b984e9b6b17fc3b966f49529
IslamAlquraishi <eslamalsayed8133@gmail.com>
2022-10-20 23:16:13 +0200

    Testting

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-// // Add Class For Links List
+// Add Class For Links List
 let linksA = document.querySelectorAll(".list_link a");
 
 linksA.forEach((link) => {
@@ -7,7 +7,6 @@ linksA.forEach((link) => {
       link.classList.remove("active");
     });
     link.classList.add("active");
-    console.log("Hello I'm Islam");
   });
 });
 
@@ -44,6 +43,14 @@ List_Link.addEventListener("mouseleave", () => {
   List_Link.classList.remove("show");
 });
 
+// Get Color In LocalStorage
+let NewColor = localStorage.getItem("Main_Color");
+
+// Check For Color In LocalStorage
+if (NewColor !== null) {
+  document.documentElement.style.setProperty("--main-color", NewColor);
+}
+
 // Change Main-Color For AllPage
 let ColorsLi = document.querySelectorAll(".data_color .list_color li");
 
@@ -54,11 +61,60 @@ ColorsLi.forEach((li) => {
     });
 
     li.classList.add("active");
-    console.log(e.target.dataset.color);
     console.log("Hello I'm Islam");
-    document.documentElement.style.setProperty(
-      "--main-color",
-      e.target.dataset.color
-    );
+    Data_Color = e.target.dataset.color;
+    document.documentElement.style.setProperty("--main-color", Data_Color);
+
+    // Set Color In LocalStorage
+    localStorage.setItem("Main_Color", Data_Color);
   });
 });
+
+let TitleHeader = document.getElementById("title_header");
+
+TitleHeader.addEventListener("click", () => {
+  window.scrollTo({
+    top: 0,
+    behavior: "smooth",
+  });
+});
+
+// Make Show Card Banner By Scroll Window
+onscroll = () => {
+  if (window.scrollY + 500 >= CradsSection.offsetTop) {
+    if (started == false) {
+      Progresss.forEach((prog) => {
+        CardsProgress(prog);
+      });
+    }
+    started = true;
+  }
+  CardFolio();
+};
+
+// Counter For Progress
+let CradsSection = document.getElementById("cards");
+let Progresss = document.querySelectorAll("#cards .progress");
+let started = false;
+
+function CardsProgress(Prog) {
+  let progress = Prog.dataset.progress;
+  let count = setInterval(() => {
+    Prog.textContent++;
+    if (Prog.textContent == progress) {
+      clearInterval(count);
+    }
+  }, 500 / progress);
+}
+
+// Scroll Window for Show Card Banner
+let Card_Block = document.querySelectorAll(".lbanner .block");
+
+function CardFolio() {
+  Card_Block.forEach((c) => {
+    if (window.scrollY + 500 >= c.offsetTop) {
+      c.classList.add("show");
+    }
+    false;
+  });
+}
